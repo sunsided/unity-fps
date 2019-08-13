c402999b1459a79ed8e438338886b21d228ac067
Markus Mayer <widemeadows@gmail.com>
2019-08-13 21:41:06 +0200

    Add Escape hotkey to stop game

---
+++ Assets/Project/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class GameController : MonoBehaviour
+{
+    void Update()
+    {
+        StopGame();
+    }
+
+    private static void StopGame()
+    {
+        if (!Input.GetButtonDown("Cancel")) return;
+
+        Debug.Log("Stopping game.");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+#else
+         Application.Quit();
+#endif
+    }
+}
