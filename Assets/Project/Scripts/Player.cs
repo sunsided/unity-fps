e38df0c83437e7fcfa643a415839d0ec12f89e31
Markus Mayer <widemeadows@gmail.com>
2019-08-13 20:56:01 +0200

    Add player movement and looking

---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// Movement speed in units per second.
+    /// </summary>
+    [Header("Movement")]
+    public float moveSpeed;
+
+    /// <summary>
+    /// Force applied upwards when jumping.
+    /// </summary>
+    public float jumpForce;
+
+    /// <summary>
+    /// Mouse look sensitivity.
+    /// </summary>
+    [Header("Camera")]
+    public float lookSensitivity;
+
+    /// <summary>
+    /// Highest up angle we can have.
+    /// </summary>
+    public float maxLookX;
+
+    /// <summary>
+    /// Lowest up angle we can have.
+    /// </summary>
+    public float minLookX;
+
+    /// <summary>
+    /// Current X rotation of the camera.
+    /// </summary>
+    private float _rotX;
+
+    /// <summary>
+    /// Main camera.
+    /// </summary>
+    private Camera _cam;
+
+    /// <summary>
+    /// The <see cref="Rigidbody"/> component.
+    /// </summary>
+    private Rigidbody _rig;
+
+    private void Awake()
+    {
+        _cam = Camera.main;
+        _rig = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+        CamLook();
+    }
+
+    void Move()
+    {
+        var x = Input.GetAxis("Horizontal") * moveSpeed;
+        var z = Input.GetAxis("Vertical") * moveSpeed;
+        _rig.velocity = new Vector3(x, _rig.velocity.y, z);
+    }
+
+    void CamLook()
+    {
+        var y = Input.GetAxis("Mouse X") * lookSensitivity;
+        var x = Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        // Y rotation (left/right) is applied to the player (not the camera!), so that the
+        // player is always facing towards the player model's forward direction.
+        transform.eulerAngles += Vector3.up * y;
+
+        // X rotation (up/down) is applied to the camera.
+        // By changing the axis sign to positive, vertical mouse motion is flipped.
+        const float axisSign = -1;
+        _rotX = Mathf.Clamp(_rotX + x, minLookX, maxLookX);
+        _cam.transform.localRotation = Quaternion.Euler(axisSign * _rotX, 0, 0);
+    }
+}
