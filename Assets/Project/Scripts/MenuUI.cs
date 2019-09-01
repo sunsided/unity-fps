3afa864a27b42242917431df53cce2a43249a2ab
Markus Mayer <widemeadows@gmail.com>
2019-09-01 15:42:27 +0200

    Add simple menu

---
+++ Assets/Project/Scripts/MenuUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project.Scripts
+{
+    public class MenuUI : MonoBehaviour
+    {
+        public void OnPlayButton()
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+        public void OnQuitButton()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+#else
+         Application.Quit();
+#endif
+        }
+    }
+}
